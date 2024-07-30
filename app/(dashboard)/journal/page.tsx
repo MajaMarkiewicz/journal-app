115f473bec4a93c42be3a6075ba3afcad955f160
Maja Moskala <m.moskala@evionica.com>
2024-07-30 09:42:30 +0200

    fix: journal entry fetching types

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,15 +1,21 @@
 import EntryCard from '@/app/components/EntryCard'
 import NewEntryCard from '@/app/components/NewEntryCard'
-import { getJournalEntries } from '@/app/lib/data'
+// import { getJournalEntriesForAUser } from '@/app/lib/data'
+// import { getUserByClerkId } from '@/utils/auth'
 
 const JournalPage = async () => {
     const entries = []
-    // const entries = await getJournalEntries()
+    // const { id: userId } = await getUserByClerkId()
+    // const entries = await getJournalEntriesForAUser(userId) // todo I should call db directly instead of hitting API
+
     return (
+    <div className='p-10 bg-zinc-500/10'>
+        <h2 className='text-3xl mb-8'>Journal</h2>
         <div className='grid grid-cols-3 gap-4 p-10'>
             <NewEntryCard />
             {entries.map(entry => <EntryCard key={entry.id} entry={entry} />)}
         </div>
+    </div>
     )
 }
 
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,8 +1,5 @@
-import { getUserByClerkId } from "@/utils/auth"
-
-export const getJournalEntries = async () => {
-    const { id } = await getUserByClerkId()
-    const res = await fetch('/api/journal', id) // @todo how to pass userId to API? fetch czy get?
+export const getJournalEntriesForAUser = async (userId: String) => {
+    const res = await fetch('/api/journal', userId) // @todo how to pass userId to API? fetch czy get?
     const data = await res.json()
     return data // @todo order by desc by createdAt
 }
