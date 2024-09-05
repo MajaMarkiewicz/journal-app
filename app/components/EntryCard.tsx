f586d2ccc89ea1da0401fe1ad61c04c59930b380
Maja Moskala <m.moskala@evionica.com>
2024-09-05 13:52:38 +0200

    fix: showing post content

--- app/components/EntryCard.tsx
+++ app/components/EntryCard.tsx
@@ -1,14 +1,16 @@
-const EntryCard = ({ entry }) => {
+import type { JournalEntryApiGet } from "@/types/journalEntry"
+
+const EntryCard: React.FC<{entry: JournalEntryApiGet}> = ({ entry }) => {
     const date = new Date(entry.createdAt).toDateString() // @todo style - choose date format
     return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
         <div className="px-4 py-5">{date}</div>
         <div className="px-4 py-5">
             <div>Main category: {entry.category}</div>
-            { entry.additionalCategory ? <span>{entry.additionalCategory}</span> : null }
+            { entry.additionalCategory ? <span>Additional category: {entry.additionalCategory}</span> : null }
         </div>
-        <div className="px-4 py-5">{entry.title}</div>
-        <div className="px-4 py-5">{entry.description}</div>
+        <strong className="px-4 py-5">{entry.title}</strong>
+        <div className="px-4 py-5">{entry.content}</div>
     </div>
     )
 }
