49eb787cd972a894772a63bd91bf60fd96e5a331
Maja Moskala <m.moskala@evionica.com>
2024-09-05 13:50:15 +0200

    feat: update entry

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,18 +1,32 @@
-import TextEditor from "@/app/components/TextEditor";
+import updateEntry from "@/app/actions/updateEntry";
+import EntryAddEditForm from "@/app/components/EntryAddEditForm";
 import JournalEntry from "@/models/JournalEntry";
+import type { JournalEntryApiGet } from "@/types/journalEntry";
 import { getUserByClerkId } from "@/utils/auth";
 import connectMongo from "@/utils/connect-mongo";
 
-const EditEntryPage = async ({ params }) => {
+const EditEntryPage: React.FC<{ params: {id: string;}}> = async ({ params }) => {
     await connectMongo()
 
     const { _id: userId } = await getUserByClerkId()
-    const entry = await JournalEntry.findOne({ _id: params.id, userId });
+    const entry = (await JournalEntry.findOne({ _id: params.id, userId }).lean() as JournalEntryApiGet)
+
+    if (!entry) {
+        return (
+          <h1 className='text-center text-2xl font-bold mt-10'>
+            Entry not found
+          </h1>
+        );
+      }
 
     return (
         <div className="h-full w-full grid grid-cols-3">
             <div className="col-span-2">
-                <TextEditor entry={entry} />
+                <EntryAddEditForm
+                    entry={entry}
+                    text='Edit entry'
+                    action={updateEntry.bind(null, params.id)}
+                />
             </div>
         </div>
     );
---
+++ app/actions/updateEntry.ts
@@ -0,0 +1,34 @@
+'use server'
+import connectMongo from "@/utils/connect-mongo"
+import JournalEntry from "@/models/JournalEntry"
+import { getUserByClerkId } from "@/utils/auth"
+import type { Category, JournalEntryApiPost } from "@/types/journalEntry"
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
+
+async function updateEntry(entryId: string, formData: FormData) {
+    await connectMongo()
+
+    const user = await getUserByClerkId()
+    if (!user || !user._id) throw new Error('User.id is missing')
+  
+    const existingEntry = await JournalEntry.findById(entryId);
+
+    if (!existingEntry) throw new Error('Property not found');
+      
+    const entryData: JournalEntryApiPost = {
+      userId: user._id,
+      title: formData.get('title') as string,
+      content: formData.get('content') as string | undefined,
+      category: formData.get('category') as Category,
+      additionalCategory: formData.get('additionalCategory') ? formData.get('additionalCategory') as Category : undefined
+    };
+  
+    await JournalEntry.findByIdAndUpdate(entryId, entryData);
+
+    revalidatePath('/')
+  
+    redirect('/journal')
+}
+
+export default updateEntry;
