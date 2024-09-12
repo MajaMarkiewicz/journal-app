cf26c5c7d05cebf1ef6bd697867f0a0461eca749
Maja Moskala <m.moskala@evionica.com>
2024-09-12 13:02:41 +0200

    feat: test new user page

--- app/(dashboard)/journal/journal.test.tsx
+++ app/(dashboard)/journal/journal.test.tsx
@@ -1,6 +1,7 @@
 it.todo('Given user has entries, when user enters journal page, then entries are displayed')
 it.todo('When user enters journal page, then add new entry functionality is available')
 
+// test by e2e
 it.todo('Given user on journal, when clicks new entry, fills the form and clicks submit, then new entry is added to database, he is redirected to journal page, and change is visible on journal page')
 it.todo('Given user on journal page with entries, when clicks an entry, change sth in the form and clicks submit, then entry is updated in database and on journal page')
 
--- app/new-user/new-user.test.tsx
+++ app/new-user/new-user.test.tsx
@@ -1,1 +1,64 @@
-it.todo('Given user logged in clerk but not in the database, when user enters the new-user page, then user is added to the database')
+import { describe, test, expect, vi, beforeEach } from 'vitest'
+import newUserPage from '@/app/new-user/page'
+import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs/server'
+import User from '@/models/User'
+import connectMongo from '@/utils/connect-mongo'
+
+vi.mock('next/navigation', () => ({
+  redirect: vi.fn(),
+}))
+
+vi.mock('@clerk/nextjs/server', () => ({
+  currentUser: vi.fn(),
+}))
+
+vi.mock('@/models/User', async (importOriginal) => {
+  const actual = await importOriginal()
+  return {
+    // @ts-expect-error
+    ...actual,
+    default: {
+    // @ts-expect-error
+    ...actual.default,
+      exists: vi.fn(),
+      create: vi.fn(),
+    },
+  }
+})
+
+vi.mock('@/utils/connect-mongo', () => ({
+  default: vi.fn(),
+}))
+
+describe('newUserPage', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  test('Given user logged in Clerk but not in the database, when user enters the new-user page, then user is added to the database', async () => {
+    const mockUser = {
+      id: 'clerk-user-id',
+      emailAddresses: [{ emailAddress: 'test@example.com' }],
+    }
+    // @ts-expect-error
+    currentUser.mockResolvedValue(mockUser)
+    // @ts-expect-error
+    connectMongo.mockResolvedValue(true)
+    // @ts-expect-error
+    User.exists.mockResolvedValue(false)
+    // @ts-expect-error
+    User.create.mockResolvedValue(true)
+
+    await newUserPage()
+
+    expect(connectMongo).toHaveBeenCalled()
+    expect(currentUser).toHaveBeenCalled()
+    expect(User.exists).toHaveBeenCalledWith({ clerkId: 'clerk-user-id' })
+    expect(User.create).toHaveBeenCalledWith({
+      clerkId: 'clerk-user-id',
+      email: 'test@example.com',
+    })
+    expect(redirect).toHaveBeenCalledWith('/journal')
+  })
+})
