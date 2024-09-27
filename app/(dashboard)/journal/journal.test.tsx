6f9a0a3eb88dd4650279553d2e4b82f74eb61c6e
Maja Moskala <m.moskala@evionica.com>
2024-09-27 16:38:39 +0200

    test: setup playright tests

--- app/(dashboard)/journal/journal.test.tsx
+++ app/(dashboard)/journal/journal.test.tsx
@@ -71,14 +71,6 @@ describe('Journal Page', () => {
   })
 })
 
-// test by e2e
-it.todo(
-  'Given user on journal, when clicks new entry, fills the form and clicks submit, then new entry is added to database, he is redirected to journal page, and change is visible on journal page',
-)
-it.todo(
-  'Given user on journal page with entries, when clicks an entry, change sth in the form and clicks submit, then entry is updated in database and on journal page',
-)
-
 // not implemented yet
 it.todo(
   'Given user on journal page with entries, when clicks an entry, then delete button is available',
